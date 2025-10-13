e20870d0331d7130c3c06db36b4edc97e4d99db1
renato-av <renato.avela.17@gmail.com>
2025-10-12 21:17:50 -0500

    refactor: streamline authentication API by consolidating GET and POST methods in signin and signout endpoints

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -2,12 +2,9 @@ import type { APIRoute } from 'astro'
 
 export const prerender = false
 
-export const handler: APIRoute = async ({ redirect, cookies }) => {
+export const POST: APIRoute = async ({ redirect, cookies }) => {
   cookies.delete('__session', {
     path: '/',
   })
   return redirect('/signin')
 }
-
-export const POST = handler
-export const GET = handler
