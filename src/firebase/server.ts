c82ab93e1cb69da024df3641d7401658c8e69e67
renato-av <renato.avela.17@gmail.com>
2025-10-12 21:47:06 -0500

    refactor: update Firebase service account initialization to use environment variables

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,27 +1,24 @@
 import type { ServiceAccount } from 'firebase-admin'
 import { initializeApp, cert, getApps } from 'firebase-admin/app'
 
+const { FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY } = process.env
+
+if (!FIREBASE_PROJECT_ID || !FIREBASE_CLIENT_EMAIL || !FIREBASE_PRIVATE_KEY) {
+  throw new Error('[firebase-admin] Missing env vars (PROJECT_ID / CLIENT_EMAIL / PRIVATE_KEY)')
+}
+
 const activeApps = getApps()
 const serviceAccount = {
   type: 'service_account',
-  project_id: import.meta.env.FIREBASE_PROJECT_ID,
-  private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: import.meta.env.FIREBASE_PRIVATE_KEY,
-  client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
-  client_id: import.meta.env.FIREBASE_CLIENT_ID,
-  auth_uri: import.meta.env.FIREBASE_AUTH_URI,
-  token_uri: import.meta.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: import.meta.env.FIREBASE_AUTH_CERT_URL,
-  client_x509_cert_url: import.meta.env.FIREBASE_CLIENT_CERT_URL,
+  project_id: FIREBASE_PROJECT_ID,
+  private_key: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: FIREBASE_CLIENT_EMAIL,
 }
 
 const initApp = () => {
-  if (import.meta.env.PROD) {
-    console.info('PROD env detected. Using default service account.')
-    // Use default config in firebase functions. Should be already injected in the server by Firebase.
-    return initializeApp()
-  }
-  console.info('Loading service account from env.')
+  console.log('[firebase-admin] Initializing Firebase Admin SDK')
+  console.log(`[firebase-admin] Project ID: ${FIREBASE_PROJECT_ID}`)
+
   return initializeApp({
     credential: cert(serviceAccount as ServiceAccount),
   })
